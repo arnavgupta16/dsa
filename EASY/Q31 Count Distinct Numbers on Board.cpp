e2f85cd2d5d2eeb0011ecef2ec706a00e5f37984
arnavgupta16 <113133432+arnavgupta16@users.noreply.github.com>
2023-12-24 01:13:29 +0530

    Create Q31 Count Distinct Numbers on Board.cpp

---
+++ EASY/Q31 Count Distinct Numbers on Board.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/count-distinct-numbers-on-board/description/
+
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        if(n==1)
+            return 1;
+        return n-1;
+        
+    }
+};
