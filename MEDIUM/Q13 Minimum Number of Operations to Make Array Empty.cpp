2e1098eaa61d1e09fb9e48ca07d0cbd2d9633d70
arnavgupta16 <113133432+arnavgupta16@users.noreply.github.com>
2024-01-04 11:32:54 +0530

    Create Q13 Minimum Number of Operations to Make Array Empty.cpp

---
+++ MEDIUM/Q13 Minimum Number of Operations to Make Array Empty.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/description/
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int, int> counts;
+        int second;
+        int result = 0;
+        for(auto i: nums){
+            counts[i]++;
+        }
+        for(auto i : counts){
+            second = i.second;
+            if(second == 1) return -1;
+            if(second % 3 == 1){
+                result += (second/3) - 1;
+                result += 2;
+            }else{
+                result += (second/3);
+                result += ((second%3)/2);
+            }
+        }
+        return result;
+    }
+};
